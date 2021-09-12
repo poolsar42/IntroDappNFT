4b802b87c018116c3edc7e9f9c6a17d3c096a8e9
poolsar42 <poolsar.42@gmail.com>
2021-09-12 22:16:27 +0300

    TDD Set up + 3 test running and passing

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Color is ERC721 {
-    constructor() ERC721("Color", "COLOR") {}
+    constructor() ERC721("Color", "COLOR") {
+
+    }
 }
